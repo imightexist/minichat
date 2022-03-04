1f8ece0d9f5dc77614689bd0b2b24a37c71a44da
imightexist <94249457+imightexist@users.noreply.github.com>
2022-03-03 19:49:14 -0600

    Update script.js

--- webserver/script.js
+++ webserver/script.js
@@ -8,3 +8,34 @@ Now I'm slidin' down the street
 The club's on top of a hill
 So I'm pickin' up speed
 */
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function usernameCookie(){
+  setCookie("username",document.getElementById('username').value,60);
+  document.getElementById("login").style.display = "none";
+}
+var socket = new WebSocket("ws://" + window.location.host);
+if (getCookie(username) = ""){
+  document.getElementById("login").style.display = "";
+}else{
+  socket.send(getCookie(username));
+}
