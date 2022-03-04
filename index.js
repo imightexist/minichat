fcd6c4336e77996ef224192e0f9fe920173ecf4f
imightexist <94249457+imightexist@users.noreply.github.com>
2022-03-03 18:18:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//packages
+const websocketServer = require('ws').WebsocketServer;
+const fs = require('fs');
+const config = require('./config.json');
+const ws = new websocketServer({port:config.port});
+const express = require('express');
+const app = express();
+var clients = [];
 
+//webserver
+if (config.webserver){
+  app.use('/',express.static(config.folder));
+  app.listen(config.port);
+}
+
+//websocket
+ws.on('connection',function(f){
+  ws.on('chat',function(e){
+    clients.forEach(function(client) {
+      client.send(message.utf8Data);
+    });
+  });
+});
