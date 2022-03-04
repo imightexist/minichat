276be6eeb262a749b5199aeb33a7037fae0c865f
imightexist <94249457+imightexist@users.noreply.github.com>
2022-03-03 18:32:28 -0600

    Create script.js

---
+++ webserver/script.js
@@ -0,0 +1,10 @@
+/*
+Now I'm breakin' all my bones
+And I'm fallin' real fast
+And I barrel towards the door
+And I break through the glass
+And I land on my ass
+Now I'm slidin' down the street
+The club's on top of a hill
+So I'm pickin' up speed
+*/
